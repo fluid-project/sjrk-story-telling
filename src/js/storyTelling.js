80b00c539d5edb8e967ce70e63c0b7b5fe5eafad
Alan Harnum <aharnum@ocadu.ca>
2017-07-25 14:53:42 -0400

    FLOE-525: add linter configuration.

--- src/js/storyTelling.js
+++ src/js/storyTelling.js
@@ -6,3 +6,11 @@ Licenses.
 You may obtain a copy of the ECL 2.0 License and BSD License at
 https://raw.githubusercontent.com/waharnum/sjrk-storyTelling/master/LICENSE.txt
 */
+
+/* global fluid, floe */
+
+(function ($, fluid) {
+
+    "use strict";
+
+})(jQuery, fluid);
