67c598a8b59bab552176afa5df25ef82837e7b84
Alan Harnum <aharnum@ocadu.ca>
2017-07-31 16:05:20 -0400

    FLOE-525: split transform tests to separate files.

---
+++ tests/js/transformsTests.js
@@ -0,0 +1,64 @@
+/*
+Copyright 2017 OCAD University
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://raw.githubusercontent.com/waharnum/sjrk-storyTelling/master/LICENSE.txt
+*/
+
+/* global fluid, jqUnit */
+
+(function ($, fluid) {
+
+    "use strict";
+
+    jqUnit.test("Test stringToArray transform function", function () {
+        jqUnit.expect(2);
+
+        var expectedArray = ["tag1","tag2"];
+
+        var stringToArrayTransform = {
+            transform: {
+                type: "sjrk.storyTelling.transforms.stringToArray",
+                inputPath: "inputString"
+            }
+        };
+
+        // TODO: THERE HAS TO BE A BETTER WAY
+        var tagArray = fluid.model.transformWithRules(
+            {inputString: "tag1,tag2"},
+            {output: stringToArrayTransform}
+            ).output;
+
+        var tagArrayNoSpace = fluid.model.transformWithRules(
+            {inputString: "tag1, tag2"},
+            {output: stringToArrayTransform}
+            ).output;
+
+        jqUnit.assertDeepEq("Generated array values are as expected", expectedArray, tagArray);
+        jqUnit.assertDeepEq("Generated array values are as expected", expectedArray, tagArrayNoSpace);
+    });
+
+    jqUnit.test("Test tagArrayToDisplayString function", function () {
+        jqUnit.expect(1);
+
+        var arrayToStringTransform = {
+            transform: {
+                type: "sjrk.storyTelling.transforms.arrayToString",
+                inputPath: "sourceArray"
+            }
+        };
+
+        var expectedString = "tag1, tag2";
+
+        // TODO: THERE HAS TO BE A BETTER WAY
+        var tagString = fluid.model.transformWithRules(
+            {sourceArray: ["tag1", "tag2"]},
+            {tagString: arrayToStringTransform}
+            ).tagString;
+
+        jqUnit.assertEquals("Generated array values are as expected", expectedString, tagString);
+    });
+
+})(jQuery, fluid);
