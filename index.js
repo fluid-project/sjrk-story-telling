e33c1c09df0cbf7ed9faa82f0c04dcd317b3982f
Alan Harnum <aharnum@ocadu.ca>
2017-08-14 11:31:49 -0400

    FLOE-525: commit initial server work.

---
+++ index.js
@@ -0,0 +1,132 @@
+/*
+Copyright 2017 OCAD University
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://raw.githubusercontent.com/waharnum/sjrk-storyTelling/master/LICENSE.txt
+*/
+
+"use strict";
+
+var fluid = require("infusion");
+var kettle = require("kettle");
+
+var sjrk = fluid.registerNamespace("sjrk");
+
+fluid.defaults("sjrk.storyTelling.server", {
+    gradeNames: "fluid.component",
+    components: {
+        server: {
+            type: "kettle.server",
+            options: {
+                port: 8081,
+                components: {
+                    ui: {
+                        type: "kettle.middleware.static",
+                        options: {
+                            "root": "./ui"
+                        }
+                    },
+                    infusionNodeModules: {
+                        type: "kettle.middleware.static",
+                        options: {
+                            "root": "./node_modules/infusion"
+                        }
+                    },
+                    binderNodeModules: {
+                        type: "kettle.middleware.static",
+                        options: {
+                            "root": "./node_modules/gpii-binder"
+                        }
+                    },
+                    storytellingNodeModules: {
+                        type: "kettle.middleware.static",
+                        options: {
+                            "root": "./node_modules/sjrk-storytelling"
+                        }
+                    },
+                    app: {
+                        type: "sjrk.storyTelling.server.app"
+                    }
+                }
+            }
+        }
+    }
+});
+
+fluid.defaults("sjrk.storyTelling.server.app", {
+    gradeNames: ["kettle.app"],
+    requestHandlers: {
+        infusionNodeModulesHandler: {
+            type: "sjrk.storyTelling.server.infusionNodeModulesHandler",
+            "route": "/*",
+            "method": "get",
+            "prefix": "/node_modules/infusion"
+        },
+        binderNodeModulesHandler: {
+            type: "sjrk.storyTelling.server.binderNodeModulesHandler",
+            "route": "/*",
+            "method": "get",
+            "prefix": "/node_modules/gpii-binder"
+        },
+        storytellingNodeModulesHandler: {
+            type: "sjrk.storyTelling.server.storytellingNodeModulesHandler",
+            "route": "/*",
+            "method": "get",
+            "prefix": "/node_modules/sjrk-storytelling"
+        },
+        uiHandler: {
+            type: "sjrk.storyTelling.server.staticHandler",
+            "route": "/*",
+            "method": "get"
+        }
+    }
+});
+
+fluid.defaults("sjrk.storyTelling.server.staticHandlerBase", {
+    gradeNames: "kettle.request.http",
+    invokers: {
+        handleRequest: {
+            funcName: "kettle.request.notFoundHandler"
+        }
+    }
+});
+
+fluid.defaults("sjrk.storyTelling.server.staticHandler", {
+    gradeNames: ["sjrk.storyTelling.server.staticHandlerBase"],
+    requestMiddleware: {
+        "static": {
+            middleware: "{server}.ui"
+        }
+    }
+});
+
+fluid.defaults("sjrk.storyTelling.server.infusionNodeModulesHandler", {
+    gradeNames: ["sjrk.storyTelling.server.staticHandlerBase"],
+    requestMiddleware: {
+        "static": {
+            middleware: "{server}.infusionNodeModules"
+        }
+    }
+});
+
+fluid.defaults("sjrk.storyTelling.server.binderNodeModulesHandler", {
+    gradeNames: ["sjrk.storyTelling.server.staticHandlerBase"],
+    requestMiddleware: {
+        "static": {
+            middleware: "{server}.binderNodeModules"
+        }
+    }
+});
+
+fluid.defaults("sjrk.storyTelling.server.storytellingNodeModulesHandler", {
+    gradeNames: ["sjrk.storyTelling.server.staticHandlerBase"],
+    requestMiddleware: {
+        "static": {
+            middleware: "{server}.storytellingNodeModules"
+        }
+    }
+});
+
+sjrk.storyTelling.server();
