2575dce4817f4bfdaf8aa50be3edbbf753d44026
Alan Harnum <aharnum@ocadu.ca>
2017-08-15 17:09:05 -0400

    FLOE-529: extract UI story loading to own file, add to FR index.

---
+++ ui/src/js/storyTellingUI.js
@@ -0,0 +1,22 @@
+// classic query string parser via
+// https://stackoverflow.com/questions/901115/how-can-i-get-query-string-values-in-javascript
+function getParameterByName(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    var regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+        results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return '';
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
+}
+
+sjrk.storyTelling.storyAuthoring.loadStoryFromQueryParam = function () {
+
+    var storyId = getParameterByName("story");
+    var storyURL = "http://localhost:8081/story/" + storyId;
+
+    $.get(storyURL, function (data) {
+        var retrievedStory = JSON.parse(data);
+        storyTelling.storyEditor.applier.change("", retrievedStory);
+    });
+}
