afa6716edaee778ab4e2397753b95bdcaf41604e
Alan Harnum <waharnum@gmail.com>
2017-11-30 13:10:45 -0500

    SJRK-40: middleware grade generation.

--- src/js/serverSetup.js
+++ src/js/serverSetup.js
@@ -10,58 +10,62 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling-server/master
 "use strict";
 
 var fluid = require("infusion");
+var sjrk = fluid.registerNamespace("sjrk");
 require("kettle");
 
+fluid.defaults("sjrk.storyTelling.server.staticMiddlewareComponents", {
+    gradeNames: ["fluid.component", "{that}.generateStaticMiddlewareComponentsGrade"],
+    invokers: {
+        generateStaticMiddlewareComponentsGrade: {
+            funcName: "sjrk.storyTelling.server.generateStaticMiddlewareComponentsGrade",
+            args: ["{that}.options.staticMiddlewareComponents"]
+        }
+    },
+    staticMiddlewareComponents: {
+        "ui": "./ui",
+        "infusionNodeModules": "./node_modules/infusion",
+        "binderNodeModules": "./node_modules/gpii-binder",
+        "storytellingNodeModules": "./node_modules/sjrk-story-telling",
+        "handlebarsNodeModules": "./node_modules/handlebars",
+        "pagedownNodeModules": "./node_modules/pagedown",
+        "gpiiHBNodeModules": "./node_modules/gpii-handlebars"
+    }
+});
+
+sjrk.storyTelling.server.generateStaticMiddlewareComponentsGrade = function (staticMiddlewareComponents) {
+    var components = {};
+    fluid.each(staticMiddlewareComponents, function (staticRoot, componentKey) {
+        var def = {
+            type: "kettle.middleware.static",
+            options: {
+                "root": staticRoot
+            }
+        };
+        components[componentKey] = def;
+    });
+
+    fluid.defaults("sjrk.storyTelling.server.staticMiddlewareComponentsGrade", {
+        components: {
+            server: {
+                options: {
+                    components: components
+                }
+            }
+        }
+    });
+
+    return "sjrk.storyTelling.server.staticMiddlewareComponentsGrade";
+
+};
+
 fluid.defaults("sjrk.storyTelling.server", {
-    gradeNames: "fluid.component",
+    gradeNames: ["fluid.component", "sjrk.storyTelling.server.staticMiddlewareComponents"],
     components: {
         server: {
             type: "kettle.server",
             options: {
                 port: 8081,
                 components: {
-                    ui: {
-                        type: "kettle.middleware.static",
-                        options: {
-                            "root": "./ui"
-                        }
-                    },
-                    infusionNodeModules: {
-                        type: "kettle.middleware.static",
-                        options: {
-                            "root": "./node_modules/infusion"
-                        }
-                    },
-                    binderNodeModules: {
-                        type: "kettle.middleware.static",
-                        options: {
-                            "root": "./node_modules/gpii-binder"
-                        }
-                    },
-                    storytellingNodeModules: {
-                        type: "kettle.middleware.static",
-                        options: {
-                            "root": "./node_modules/sjrk-story-telling"
-                        }
-                    },
-                    handlebarsNodeModules: {
-                        type: "kettle.middleware.static",
-                        options: {
-                            "root": "./node_modules/handlebars"
-                        }
-                    },
-                    pagedownNodeModules: {
-                        type: "kettle.middleware.static",
-                        options: {
-                            "root": "./node_modules/pagedown"
-                        }
-                    },
-                    gpiiHBNodeModules: {
-                        type: "kettle.middleware.static",
-                        options: {
-                            "root": "./node_modules/gpii-handlebars"
-                        }
-                    },
                     app: {
                         type: "sjrk.storyTelling.server.app"
                     },
