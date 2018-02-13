06196f4a25baff37f5f6a8a56f983980955270fe
Alan Harnum <waharnum@gmail.com>
2018-02-13 12:25:13 -0500

    SJRK-49: basic image uploader implementation.

--- src/js/storyTelling-block-image.js
+++ src/js/storyTelling-block-image.js
@@ -13,6 +13,62 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
 
     "use strict";
 
+    // A simple single-file uploader using what's presumed to be a hidden
+    // <input type="file">
+    //
+    // This component is expected to be used with existing markup
+    fluid.defaults("sjrk.storyTelling.block.singleFileUploader", {
+        gradeNames: ["fluid.viewComponent"],
+        members: {
+            currentFile: null,
+        },
+        // Holds a fileObjectURL for file preview purposes
+        model: {
+            fileObjectURL: null
+        },
+        events: {
+            onUploadRequested: null,
+            onFileChanged: null
+        },
+        selectors: {
+            fileInput: ".sjrkc-storyblock-uploader-input"
+        },
+        listeners: {
+            "onCreate.addFileInputChangeListener": {
+                "this": "{that}.dom.fileInput",
+                "method": "change",
+                "args": ["{that}.handleFileInputChange"]
+            },
+            "onUploadRequested.clickFileInput": {
+                "this": "{that}.dom.fileInput",
+                "method": "click",
+                "args": []
+            },
+            "onFileChanged.updateFileObjectURL": {
+                "func": "sjrk.storyTelling.block.singleFileUploader.updateFileObjectURL",
+                "args": ["{that}", "{that}.currentFile"]
+            }
+        },
+        invokers: {
+            "handleFileInputChange": {
+                funcName: "sjrk.storyTelling.block.singleFileUploader.handleFileInputChange",
+                args: ["{that}", "{that}.dom.fileInput"]
+            }
+        }
+    });
+
+    sjrk.storyTelling.block.singleFileUploader.handleFileInputChange = function (that, fileInput) {
+        var fileList = fileInput[0].files;
+        var currentFile = fileList[0];
+        that.currentFile = currentFile;
+        that.events.onFileChanged.fire();
+    };
+
+    sjrk.storyTelling.block.singleFileUploader.updateFileObjectURL = function (that, currentFile) {
+        URL.revokeObjectURL(that.model.fileObjectURL);
+        that.applier.change("fileObjectURL", URL.createObjectURL(currentFile));
+    };
+
     fluid.defaults("sjrk.storyTelling.block.image", {
         gradeNames: ["sjrk.storyTelling.block"],
         model: {
@@ -31,6 +87,28 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
             imageAltText: ".sjrkc-storyblock-image-alt-text",
             imageDescription: ".sjrck-storyblock-image-description"
         },
+        listeners: {
+            "{templateManager}.events.onTemplateRendered": [{
+                this: "{that}.dom.imageUploadButton",
+                method: "click",
+                args: ["{that}.events.imageUploadRequested.fire"],
+                namespace: "bindImageUploadRequested"
+            }, {
+                this: "{that}.dom.imageCaptureButton",
+                method: "click",
+                args: ["{that}.events.imageCaptureRequested.fire"],
+                namespace: "bindImageCaptureRequested"
+            }],
+            "imageCaptureRequested.handleImageCaptureRequested": {
+                func: "sjrk.storyTelling.block.image.handleCaptureRequested"
+            }
+        },
+        invokers: {
+            "updateImagePreview": {
+                funcName: "sjrk.storyTelling.block.image.updateImagePreview",
+                args: ["{that}", "{singleFileUploader}.model.fileObjectURL"]
+            }
+        },
         components: {
             templateManager: {
                 options: {
@@ -42,8 +120,8 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
                             // TODO: think about the usage/location of this function
                             imageUploadButton: "@expand:sjrk.storyTelling.ui.getLabelId(storyBlockImageUploadButton)",
                             imageCaptureButton: "@expand:sjrk.storyTelling.ui.getLabelId(storyBlockImageCaptureButton)",
-                            imageAltText: "@expand:sjrk.storyTelling.ui.getLabelId(storyBlockImageAltText)",
-                            imageDescription: "@expand:sjrk.storyTelling.ui.getLabelId(storyBlockImageDescription)"
+                            imageAltTextIdForLabel: "@expand:sjrk.storyTelling.ui.getLabelId(storyBlockImageAltText)",
+                            imageDescriptionIdForLabel: "@expand:sjrk.storyTelling.ui.getLabelId(storyBlockImageDescription)"
                         }
                     }
                 }
@@ -52,11 +130,39 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
                 options: {
                     selectors: "{block}.options.selectors",
                     bindings: {
-                        textBlock: "text"
+                        imageAltText: "alternativeText",
+                        imageDescription: "description"
+                    }
+                }
+            },
+            singleFileUploader: {
+                type: "sjrk.storyTelling.block.singleFileUploader",
+                createOnEvent: "{templateManager}.events.onTemplateRendered",
+                container: ".sjrkc-storyblock-uploader",
+                options: {
+                    listeners: {
+                        "{image}.events.imageUploadRequested": {
+                            func: "{that}.events.onUploadRequested.fire"
+                        }
+                    },
+                    modelListeners: {
+                        "fileObjectURL": {
+                            func: "{image}.updateImagePreview",
+                            excludeSource: "init"
+                        }
                     }
                 }
             }
         }
     });
+
+    sjrk.storyTelling.block.image.updateImagePreview = function (that, imagePreviewURL) {
+        that.locate("imagePreview").attr("src", imagePreviewURL);
+    };
+
+    // TODO: placeholder
+    sjrk.storyTelling.block.image.handleCaptureRequested = function () {
+        console.log("sjrk.storyTelling.block.image.handleCaptureRequested");
+    };
 
 })(jQuery, fluid);
---
+++ tests/js/storyTelling-block-imageTests.js
@@ -0,0 +1,86 @@
+/*
+Copyright 2018 OCAD University
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENSE.txt
+*/
+
+/* global fluid */
+
+(function ($, fluid) {
+
+    "use strict";
+
+    fluid.defaults("sjrk.storyTelling.block.testImage", {
+        gradeNames: ["sjrk.storyTelling.block.image"],
+        components: {
+            templateManager: {
+                options: {
+                    templateConfig: {
+                        resourcePrefix: "../.."
+                    }
+                }
+            }
+        }
+    });
+    //
+    // fluid.defaults("sjrk.storyTelling.block.textTester", {
+    //     gradeNames: ["fluid.test.testCaseHolder"],
+    //     modules: [{
+    //         name: "Test Text Block.",
+    //         tests: [{
+    //             name: "Test Text Block",
+    //             expect: 3,
+    //             sequence: [{
+    //                 event: "{textTest text}.events.onReadyToBind",
+    //                 listener: "jqUnit.assert",
+    //                 args: ["The template has been loaded and rendered"]
+    //             },
+    //             {
+    //                 funcName: "sjrk.storyTelling.testUtils.changeFormElement",
+    //                 args: ["{text}", "textBlock", "Hello Shyguy!"]
+    //             },
+    //             {
+    //                 changeEvent: "{text}.applier.modelChanged",
+    //                 path: "text",
+    //                 listener: "jqUnit.assertEquals",
+    //                 args: ["The model text has expected value", "Hello Shyguy!", "{text}.model.text"]
+    //             },
+    //             {
+    //                 func: "{text}.applier.change",
+    //                 args: ["text", "Hello Rootbeer!"]
+    //             },
+    //             {
+    //                 changeEvent: "{text}.applier.modelChanged",
+    //                 path: "text",
+    //                 listener: "sjrk.storyTelling.testUtils.assertElementValue",
+    //                 args: ["{text}.dom.textBlock", "Hello Rootbeer!"]
+    //             }]
+    //         }]
+    //     }]
+    // });
+    //
+    // fluid.defaults("sjrk.storyTelling.block.textTest", {
+    //     gradeNames: ["fluid.test.testEnvironment"],
+    //     components: {
+    //         text: {
+    //             type: "sjrk.storyTelling.block.testText",
+    //             container: "#testText",
+    //             createOnEvent: "{textTester}.events.onTestCaseStart"
+    //         },
+    //         textTester: {
+    //             type: "sjrk.storyTelling.block.textTester"
+    //         }
+    //     }
+    // });
+    //
+    $(document).ready(function () {
+        // fluid.test.runTests([
+        //     "sjrk.storyTelling.block.textTest"
+        // ]);
+        sjrk.storyTelling.block.testImage("#testImage");
+    });
+
+})(jQuery, fluid);
