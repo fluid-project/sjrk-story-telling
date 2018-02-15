6f46fde1300291125a24a936fbedd5afcee65d20
BlueSlug <mossmonkey@gmail.com>
2018-02-15 16:00:00 -0500

    SJRK-49: added blockManager to blockEditor and wired it up

--- src/js/storyTelling-ui-blockEditor.js
+++ src/js/storyTelling-ui-blockEditor.js
@@ -16,14 +16,68 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
     fluid.defaults("sjrk.storyTelling.ui.blockEditor", {
         gradeNames: ["sjrk.storyTelling.ui"],
         selectors: {
-            storyTitle: ".sjrkc-storyTelling-storyTitle",
-            storyAuthor: ".sjrkc-storyTelling-storyAuthor",
-            storyTags: ".sjrkc-storyTelling-storyTags"
+            storySubmit: ".sjrkc-storyTelling-storySubmit",
+            storyEditorNext: ".sjrkc-storyTelling-storyEditorNext",
+            storyEditorPrevious: ".sjrkc-storyTelling-storyEditorPrevious",
+            storyEditorPage1: ".sjrkc-storyTelling-storyEditorPage1",
+            storyEditorPage2: ".sjrkc-storyTelling-storyEditorPage2",
+            storyAddTextBlock: ".sjrkc-storyTelling-button-text-block",
+            storyAddImageBlock: ".sjrkc-storyTelling-button-image-block",
+            storyRemoveSelectedBlocks: ".sjrkc-storyTelling-button-remove-blocks",
+            storyRestoreRemovedBlocks: ".sjrkc-storyTelling-button-restore-blocks"
         },
         interfaceControlStrings: {
             storyTitleIdForLabel: "@expand:{that}.getLabelId(title)",
             storyAuthorIdForLabel: "@expand:{that}.getLabelId(author)",
             storyTagsIdForLabel: "@expand:{that}.getLabelId(tags)"
+        },
+        events: {
+            onStorySubmitRequested: null,
+            onEditorNextRequested: null,
+            onEditorPreviousRequested: null,
+            onStoryListenToRequested: null,
+            onTextBlockAdditionRequested: null,
+            onImageBlockAdditionRequested: null,
+            onRemoveBlocksRequested: null,
+            onRestoreBlocksRequested: null
+        },
+        listeners: {
+            "onReadyToBind.bindAddTextBlock": {
+                "this": "{that}.dom.storyAddTextBlock",
+                "method": "click",
+                "args": ["{that}.events.onTextBlockAdditionRequested.fire"]
+            },
+            "onReadyToBind.bindSubmitControl": {
+                "this": "{that}.dom.storySubmit",
+                "method": "click",
+                "args": ["{that}.events.onStorySubmitRequested.fire"]
+            },
+            "onReadyToBind.bindEditorNextControl": {
+                "this": "{that}.dom.storyEditorNext",
+                "method": "click",
+                "args": ["{that}.events.onEditorNextRequested.fire"]
+            },
+            "onReadyToBind.bindEditorPreviousControl": {
+                "this": "{that}.dom.storyEditorPrevious",
+                "method": "click",
+                "args": ["{that}.events.onEditorPreviousRequested.fire"]
+            },
+            "onEditorNextRequested.manageVisibility": {
+                funcName: "sjrk.storyTelling.ui.manageVisibility",
+                args: [
+                    ["{that}.dom.storyEditorPage1"],
+                    ["{that}.dom.storyEditorPage2"],
+                    "{that}.events.onVisibilityChanged"
+                ]
+            },
+            "onEditorPreviousRequested.manageVisibility": {
+                funcName: "sjrk.storyTelling.ui.manageVisibility",
+                args: [
+                    ["{that}.dom.storyEditorPage2"],
+                    ["{that}.dom.storyEditorPage1"],
+                    "{that}.events.onVisibilityChanged"
+                ]
+            }
         },
         // TODO: add events for binding the various buttons
         // TODO: add listeners to handle the various buttons' functionality
@@ -32,6 +86,25 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
                 options: {
                     templateConfig: {
                         templatePath: "%resourcePrefix/src/templates/storyBlockEditor.handlebars"
+                    }
+                }
+            },
+            blockManager: {
+                type: "sjrk.dynamicViewComponentManager",
+                // container: "{ui}.options.selectors.storyContent",
+                container: "{ui}.options.selectors.storyContent",
+                createOnEvent: "{templateManager}.events.onAllResourcesLoaded",
+                options: {
+                    dynamicComponents: {
+                        managedViewComponents: {
+                            type: "sjrk.storyTelling.block.textBlock"
+                        }
+                    },
+                    listeners: {
+                        "{blockEditor}.events.onTextBlockAdditionRequested": {
+                            func: "{that}.events.viewComponentContainerRequested",
+                            namespace: "addItem"
+                        }
                     }
                 }
             },
