e2896ab27237fcfdb3e1c9ab77b835fb246bd23e
Alan Harnum <waharnum@gmail.com>
2018-02-23 10:53:52 -0500

    SJRK-49: start building learning reflections site structure.

---
+++ sites/learning-reflections/src/js/learningReflections.js
@@ -0,0 +1,48 @@
+/*
+Copyright 2018 OCAD University
+Licensed under the Educational Community License (ECL), Version 2.0 or the New
+BSD license. You may not use this file except in compliance with one these
+Licenses.
+You may obtain a copy of the ECL 2.0 License and BSD License at
+https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENSE.txt
+*/
+
+/* global fluid */
+
+(function ($, fluid) {
+
+    "use strict";
+
+    fluid.defaults("sjrk.storyTelling.ui.blockEditor.learningReflections", {
+        gradeNames: ["sjrk.storyTelling.ui.blockEditor"],
+        components: {
+            templateManager: {
+                options: {
+                    templateConfig: {
+                        resourcePrefix: "../.."
+                    }
+                }
+            },
+            blockManager: {
+                options: {
+                    dynamicComponents: {
+                        managedViewComponents: {
+                            options: {
+                                components: {
+                                    templateManager: {
+                                        options: {
+                                            templateConfig: {
+                                                resourcePrefix: "../..",
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    });
+
+})(jQuery, fluid);
