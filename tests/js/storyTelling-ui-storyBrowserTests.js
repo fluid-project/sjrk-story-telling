a1a17a9d62fbb59151580cc3b461fc287afaa4cf
BlueSlug <mossmonkey@gmail.com>
2018-06-06 17:53:07 -0400

    SJRK-140: added tests for storyBrowser view changing behaviour

--- tests/js/storyTelling-ui-storyBrowserTests.js
+++ tests/js/storyTelling-ui-storyBrowserTests.js
@@ -7,7 +7,7 @@ You may obtain a copy of the ECL 2.0 License and BSD License at
 https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENSE.txt
 */
 
-/* global fluid */
+/* global fluid, sjrk, jqUnit */
 
 (function ($, fluid) {
 
@@ -28,11 +28,17 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
 
     fluid.defaults("sjrk.storyTelling.ui.storyBrowserTester", {
         gradeNames: ["fluid.test.testCaseHolder"],
+        invokers: {
+            verifyViewSetting: {
+                funcName: "sjrk.storyTelling.ui.storyBrowserTester.verifyViewSetting",
+                args: ["{storyBrowser}.dom.viewList", "{arguments}.0", "{storyBrowser}.options.browserConfig.gridViewClassName"]
+            }
+        },
         modules: [{
             name: "Test Story Browser UI.",
             tests: [{
                 name: "Test UI controls",
-                expect: 3,
+                expect: 13,
                 sequence: [{
                     "event": "{storyBrowserTest storyBrowser}.events.onControlsBound",
                     listener: "jqUnit.assert",
@@ -43,22 +49,127 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
                     "element": "{storyBrowser}.dom.gridViewLink"
                 },
                 {
-                    "event": "{storyBrowser}.events.onGridViewRequested",
+                    "event": "{storyBrowser}.events.onViewChangeRequested",
                     listener: "jqUnit.assert",
-                    args: "onGridViewRequested event fired."
+                    args: "onViewChangeRequested event fired."
+                },
+                {
+                    func: "{that}.verifyViewSetting",
+                    args: ["grid"]
                 },
                 {
                     "jQueryTrigger": "click",
                     "element": "{storyBrowser}.dom.listViewLink"
                 },
                 {
-                    "event": "{storyBrowser}.events.onListViewRequested",
+                    "event": "{storyBrowser}.events.onViewChangeRequested",
                     listener: "jqUnit.assert",
-                    args: "onListViewRequested event fired."
+                    args: "onViewChangeRequested event fired."
+                },
+                {
+                    func: "{that}.verifyViewSetting",
+                    args: ["list"]
+                },
+                {
+                    func: "{storyBrowser}.changeView",
+                    args: ["grid"]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["grid"]
+                },
+                {
+                    func: "{storyBrowser}.changeView",
+                    args: ["list"]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["list"]
+                },
+                {
+                    func: "{storyBrowser}.changeView",
+                    args: [undefined]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["list"]
+                },
+                {
+                    func: "{storyBrowser}.changeView",
+                    args: [5]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["list"]
+                },
+                {
+                    func: "{storyBrowser}.changeView",
+                    args: [{}]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["list"]
+                },
+                {
+                    func: "{storyBrowser}.changeView",
+                    args: [false]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["list"]
+                },
+                {
+                    funcName: "sjrk.storyTelling.ui.storyBrowser.changeView",
+                    args: ["{storyBrowser}",
+                        "viewList",
+                        "grid",
+                        "{storyBrowser}.options.browserConfig.gridViewClassName",
+                        "{storyBrowser}.events.onViewChanged"]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["grid"]
+                },
+                {
+                    funcName: "sjrk.storyTelling.ui.storyBrowser.changeView",
+                    args: ["{storyBrowser}",
+                        "viewList",
+                        "list",
+                        "{storyBrowser}.options.browserConfig.gridViewClassName",
+                        "{storyBrowser}.events.onViewChanged"]
+                },
+                {
+                    "event": "{storyBrowser}.events.onViewChanged",
+                    func: "{that}.verifyViewSetting",
+                    args: ["list"]
                 }]
             }]
         }]
     });
+
+    sjrk.storyTelling.ui.storyBrowserTester.verifyViewSetting = function (viewListElement, expectedViewSetting, gridViewClassName) {
+        var classesContainGridClass = false;
+
+        fluid.each(viewListElement.attr("class").split(" "), function (className) {
+            if (className === gridViewClassName) {
+                classesContainGridClass = true;
+            }
+        });
+
+        if ((classesContainGridClass && expectedViewSetting === gridViewClassName) ||
+            (!classesContainGridClass && expectedViewSetting !== gridViewClassName)) {
+            jqUnit.assert("The view setting is as expected");
+        } else {
+            jqUnit.fail("The view setting is not as expected");
+        }
+    };
 
     fluid.defaults("sjrk.storyTelling.ui.storyBrowserTest", {
         gradeNames: ["fluid.test.testEnvironment"],
