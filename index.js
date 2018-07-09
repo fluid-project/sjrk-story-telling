dc164ecf8228b5e203560970cef316333c473cda
BlueSlug <mossmonkey@gmail.com>
2018-07-09 18:33:26 -0400

    SJRK-158: updated licence info and LICENSE.txt to remove ECL references

--- index.js
+++ index.js
@@ -1,9 +1,7 @@
 /*
 Copyright 2017 OCAD University
-Licensed under the Educational Community License (ECL), Version 2.0 or the New
-BSD license. You may not use this file except in compliance with one these
-Licenses.
-You may obtain a copy of the ECL 2.0 License and BSD License at
+Licensed under the New BSD license. You may not use this file except in compliance with this licence.
+You may obtain a copy of the BSD License at
 https://raw.githubusercontent.com/fluid-project/sjrk-story-telling-server/master/LICENSE.txt
 */
 
