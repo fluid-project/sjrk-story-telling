a89177e72618b9cc88734e5993b6658259f6415d
BlueSlug <mossmonkey@gmail.com>
2019-02-28 18:32:27 -0500

    SJRK-223: fixed server tests

--- tests/testServer.js
+++ tests/testServer.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 OCAD University
+Copyright 2018-2019 OCAD University
 Licensed under the New BSD license. You may not use this file except in compliance with this licence.
 You may obtain a copy of the BSD License at
 https://raw.githubusercontent.com/fluid-project/sjrk-story-telling-server/master/LICENSE.txt
@@ -12,14 +12,14 @@ var fluid = require("infusion"),
     kettle = require("kettle"),
     jqUnit = fluid.registerNamespace("jqUnit");
 
-require("../src/js/staticHandlerBase");
-require("../src/js/middleware/basicAuth");
-require("../src/js/middleware/saveStoryWithBinaries");
-require("../src/js/middleware/staticMiddlewareSubdirectoryFilter");
-require("../src/js/dataSource");
-require("../src/js/serverSetup");
-require("../src/js/requestHandlers");
-require("../src/js/serverSetup");
+require("../server/src/js/staticHandlerBase");
+require("../server/src/js/middleware/basicAuth");
+require("../server/src/js/middleware/saveStoryWithBinaries");
+require("../server/src/js/middleware/staticMiddlewareSubdirectoryFilter");
+require("../server/src/js/dataSource");
+require("../server/src/js/serverSetup");
+require("../server/src/js/requestHandlers");
+require("../server/src/js/serverSetup");
 
 kettle.loadTestingSupport();
 
@@ -36,7 +36,7 @@ sjrk.storyTelling.server.testServerDefs = [{
         validNodeModulesRequest: {
             type: "kettle.test.request.http",
             options: {
-                path: "/node_modules/sjrk-story-telling/src/js/dynamicViewComponentManager.js",
+                path: "/node_modules/infusion/src/framework/preferences/js/Panels.js",
                 method: "GET"
             }
         },
--- tests/testServerWithStorage.js
+++ tests/testServerWithStorage.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 OCAD University
+Copyright 2018-2019 OCAD University
 Licensed under the New BSD license. You may not use this file except in compliance with this licence.
 You may obtain a copy of the BSD License at
 https://raw.githubusercontent.com/fluid-project/sjrk-story-telling-server/master/LICENSE.txt
@@ -13,15 +13,15 @@ var fluid = require("infusion"),
     fs = require("fs"),
     jqUnit = fluid.registerNamespace("jqUnit");
 
-require("../src/js/staticHandlerBase");
-require("../src/js/middleware/basicAuth");
-require("../src/js/middleware/saveStoryWithBinaries");
-require("../src/js/middleware/staticMiddlewareSubdirectoryFilter");
-require("../src/js/dataSource");
-require("../src/js/serverSetup");
-require("../src/js/requestHandlers");
-require("../src/js/serverSetup");
-require("../src/js/db/story-dbConfiguration");
+require("../server/src/js/staticHandlerBase");
+require("../server/src/js/middleware/basicAuth");
+require("../server/src/js/middleware/saveStoryWithBinaries");
+require("../server/src/js/middleware/staticMiddlewareSubdirectoryFilter");
+require("../server/src/js/dataSource");
+require("../server/src/js/serverSetup");
+require("../server/src/js/requestHandlers");
+require("../server/src/js/serverSetup");
+require("../server/src/js/db/story-dbConfiguration");
 
 kettle.loadTestingSupport();
 
