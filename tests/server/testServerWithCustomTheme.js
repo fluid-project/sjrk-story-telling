a0befaa867ab624b24735d5a67312692217c0b4a
BlueSlug <mossmonkey@gmail.com>
2019-07-15 14:56:50 -0400

    SJRK-237: fixed custom theme server test and removed extraneous images

--- tests/server/testServerWithCustomTheme.js
+++ tests/server/testServerWithCustomTheme.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2018-2019 OCAD University
+Copyright 2019 OCAD University
 Licensed under the New BSD license. You may not use this file except in compliance with this licence.
 You may obtain a copy of the BSD License at
 https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENSE.txt
