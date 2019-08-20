a92c39e36b631760d05b7f46b9c8ab420759b180
BlueSlug <mossmonkey@gmail.com>
2019-08-19 23:29:07 -0400

    NOJIRA: updated arrayToString transform tests, added more test cases

--- tests/ui/js/transformsTests.js
+++ tests/ui/js/transformsTests.js
@@ -1,5 +1,5 @@
 /*
-Copyright 2018 OCAD University
+Copyright 2017-2019 OCAD University
 Licensed under the New BSD license. You may not use this file except in compliance with this licence.
 You may obtain a copy of the BSD License at
 https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENSE.txt
@@ -37,24 +37,102 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
         jqUnit.assertDeepEq("Generated array values are as expected", expectedArray, tagArrayNoSpace);
     });
 
-    jqUnit.test("Test tagArrayToDisplayString function", function () {
-        jqUnit.expect(1);
+    var arrayToStringTransformTestCases = [
+        {
+            sourceArray: ["Shyguy", "Rootbeer"],
+            rules: {},
+            expectedResult: "Shyguy, Rootbeer"
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer"],
+            rules: {
+                separator: ". "
+            },
+            expectedResult: "Shyguy. Rootbeer"
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer"],
+            rules: {
+                separator: true
+            },
+            expectedResult: "ShyguytrueRootbeer"
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer"],
+            rules: {
+                separator: 1
+            },
+            expectedResult: "Shyguy1Rootbeer"
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer"],
+            rules: {
+                trailingSeparator: true
+            },
+            expectedResult: "Shyguy, Rootbeer, "
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer", 0, {somethingElse: ""}, ["a string in an array"]],
+            rules: {},
+            expectedResult: "Shyguy, Rootbeer"
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer", 0, {somethingElse: ""}, ["a string in an array"]],
+            rules: {
+                stringOnly: false
+            },
+            expectedResult: "Shyguy, Rootbeer, 0, [object Object], a string in an array"
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer"],
+            rules: {
+                path: "innerPath"
+            },
+            expectedResult: ""
+        },
+        {
+            sourceArray: [
+                { innerPath: "Shyguy", ignoredValue: "Not a cat" },
+                { innerPath: "Rootbeer", ignoredValue: "Also not a cat" }
+            ],
+            rules: {
+                path: "innerPath"
+            },
+            expectedResult: "Shyguy, Rootbeer"
+        },
+        {
+            sourceArray: [
+                { innerPath: "Shyguy", ignoredValue: "Not a cat" },
+                { innerPath: "Rootbeer", ignoredValue: "Also not a cat" }
+            ],
+            rules: {
+                path: "notARealPath"
+            },
+            expectedResult: ""
+        },
+        {
+            sourceArray: ["Shyguy", "Rootbeer", "", false, {}, []],
+            rules: {},
+            expectedResult: "Shyguy, Rootbeer"
+        }
+    ];
 
-        var arrayToStringTransform = {
-            transform: {
+    jqUnit.test("Test arrayToString transform function", function () {
+        jqUnit.expect(arrayToStringTransformTestCases.length);
+
+        fluid.each(arrayToStringTransformTestCases, function (testCase, index) {
+            var transformRules = $.extend({}, testCase.rules, {
                 type: "sjrk.storyTelling.transforms.arrayToString",
-                inputPath: "sourceArray"
-            }
-        };
+                inputPath: "sourceArray",
+            })
 
-        var expectedString = "tag1, tag2";
+            var resultString = fluid.model.transformWithRules(
+                { sourceArray: testCase.sourceArray },
+                { resultString: { transform: transformRules }}
+            ).resultString;
 
-        var tagString = fluid.model.transformWithRules(
-            {sourceArray: ["tag1", "tag2"]},
-            {tagString: arrayToStringTransform}
-        ).tagString;
-
-        jqUnit.assertEquals("Generated array values are as expected", expectedString, tagString);
+            jqUnit.assertEquals("Generated array values for test case " + index + " are as expected", testCase.expectedResult, resultString);
+        });
     });
 
 })(jQuery, fluid);
