6715827f8834010f4ce37c6fe6edcf61f16b5956
BlueSlug <mossmonkey@gmail.com>
2019-08-27 18:42:22 -0400

    SJRK-263: added inverted transform tests

--- tests/ui/js/transformsTests.js
+++ tests/ui/js/transformsTests.js
@@ -11,158 +11,181 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
 
 (function ($, fluid) {
 
-    var stringToArrayTransformTestCases = [
-        {
+    var stringToArrayTransformTestCases = {
+        "test_01": {
             input: "Shyguy,Rootbeer",
             rules: {},
             expectedResult: ["Shyguy", "Rootbeer"],
             expectedInverseResult: "Shyguy, Rootbeer"
         },
-        {
+        "test_02": {
             input: "Shyguy, Rootbeer",
             rules: {},
             expectedResult: ["Shyguy", "Rootbeer"],
             expectedInverseResult: "Shyguy, Rootbeer"
         },
-        {
+        "test_03": {
             input: "Shyguy   , Rootbeer",
             rules: {
                 trim: false
             },
             expectedResult: ["Shyguy   ", " Rootbeer"],
-            expectedInverseResult: "Shyguy   , Rootbeer"
+            expectedInverseResult: "Shyguy   ,  Rootbeer"
         },
-        {
+        "test_04": {
             input: "Shyguy, Rootbeer",
             rules: {
                 delimiter: "."
             },
-            expectedResult: ["Shyguy, Rootbeer"]
+            expectedResult: ["Shyguy, Rootbeer"],
+            expectedInverseResult: "Shyguy, Rootbeer"
         },
-        {
+        "test_05": {
             input: "Shyguy. Rootbeer",
             rules: {
                 delimiter: "."
             },
-            expectedResult: ["Shyguy", "Rootbeer"]
+            expectedResult: ["Shyguy", "Rootbeer"],
+            expectedInverseResult: "Shyguy.Rootbeer"
         },
-        {
+        "test_06": {
             input: ["Shyguy ,Rootbeer"],
             rules: {},
-            expectedResult: []
+            expectedResult: [],
+            expectedInverseResult: ""
         },
-        {
+        "test_07": {
             input: 0,
             rules: {},
-            expectedResult: []
+            expectedResult: [],
+            expectedInverseResult: ""
         },
-        {
+        "test_08": {
             input: {},
             rules: {},
-            expectedResult: []
+            expectedResult: [],
+            expectedInverseResult: ""
         },
-        {
+        "test_09": {
             input: [],
             rules: {},
-            expectedResult: []
+            expectedResult: [],
+            expectedInverseResult: ""
         },
-        {
+        "test_10": {
             input: false,
             rules: {},
-            expectedResult: []
+            expectedResult: [],
+            expectedInverseResult: ""
         },
-        {
+        "test_11": {
             input: "",
             rules: {},
-            expectedResult: []
+            expectedResult: [],
+            expectedInverseResult: ""
         },
-        {
+        "test_12": {
             input: null,
             rules: {},
-            expectedResult: []
+            expectedResult: [],
+            expectedInverseResult: ""
         },
-        {
+        "test_13": {
             input: undefined,
             rules: {},
-            expectedResult: undefined
+            expectedResult: undefined,
+            expectedInverseResult: undefined
         }
-    ];
+    };
 
     jqUnit.test("Test stringToArray transform function", function () {
-        jqUnit.expect(stringToArrayTransformTestCases.length);
+        jqUnit.expect(26);
 
         fluid.each(stringToArrayTransformTestCases, function (testCase, index) {
-            var transformRules = $.extend({}, testCase.rules, {
+            var transformSpec = $.extend({}, testCase.rules, {
                 type: "sjrk.storyTelling.transforms.stringToArray",
                 inputPath: "inputString"
             });
 
-            var transform = {
-                transform: transformRules
+            var transformRules = {
+                transform: transformSpec
             };
 
             var output = fluid.model.transformWithRules(
                 { inputString: testCase.input },
-                { output: transform }
+                { output: transformRules }
             ).output;
-
             jqUnit.assertDeepEq("Generated array values for test case " + index + " are as expected", testCase.expectedResult, output);
 
+            var invertedRules = fluid.model.transform.invertConfiguration(transformRules);
+            var invertedOutput = fluid.model.transformWithRules(
+                output,
+                invertedRules
+            ).inputString;
+            jqUnit.assertEquals("Generated inverted string value for test case " + index + " is as expected", testCase.expectedInverseResult, invertedOutput);
         });
     });
 
-    var arrayToStringTransformTestCases = [
-        {
+    var arrayToStringTransformTestCases = {
+        "test_01": {
             input: ["Shyguy", "Rootbeer"],
             rules: {},
-            expectedResult: "Shyguy, Rootbeer"
+            expectedResult: "Shyguy, Rootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_02": {
             input: {cat1: "Shyguy", cat2: "Rootbeer"},
             rules: {},
-            expectedResult: "Shyguy, Rootbeer"
+            expectedResult: "Shyguy, Rootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_03": {
             input: ["Shyguy", "Rootbeer"],
             rules: {
                 delimiter: ". "
             },
-            expectedResult: "Shyguy. Rootbeer"
+            expectedResult: "Shyguy. Rootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_04": {
             input: ["Shyguy", "Rootbeer"],
             rules: {
                 delimiter: true
             },
-            expectedResult: "ShyguytrueRootbeer"
+            expectedResult: "ShyguytrueRootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_05": {
             input: ["Shyguy", "Rootbeer"],
             rules: {
                 delimiter: 1
             },
-            expectedResult: "Shyguy1Rootbeer"
+            expectedResult: "Shyguy1Rootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_06": {
             input: ["Shyguy", "Rootbeer", 0, {somethingElse: ""}, ["a string in an array"]],
             rules: {},
-            expectedResult: "Shyguy, Rootbeer"
+            expectedResult: "Shyguy, Rootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_07": {
             input: ["Shyguy", "Rootbeer", 0, {somethingElse: ""}, ["a string in an array"]],
             rules: {
                 stringOnly: false
             },
-            expectedResult: "Shyguy, Rootbeer, 0, [object Object], a string in an array"
+            expectedResult: "Shyguy, Rootbeer, 0, [object Object], a string in an array",
+            expectedInverseResult: ["Shyguy", "Rootbeer", "0", "[object Object]", "a string in an array"]
         },
-        {
+        "test_08": {
             input: ["Shyguy", "Rootbeer"],
             rules: {
                 path: "innerPath"
             },
-            expectedResult: ""
+            expectedResult: "",
+            expectedInverseResult: []
         },
-        {
+        "test_09": {
             input: [
                 { innerPath: "Shyguy", ignoredValue: "Not a cat" },
                 { innerPath: "Rootbeer", ignoredValue: "Also not a cat" }
@@ -170,9 +193,10 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
             rules: {
                 path: "innerPath"
             },
-            expectedResult: "Shyguy, Rootbeer"
+            expectedResult: "Shyguy, Rootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_10": {
             input: [
                 { innerPath: "Shyguy", ignoredValue: "Not a cat" },
                 { innerPath: "Rootbeer", ignoredValue: "Also not a cat" }
@@ -180,60 +204,78 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
             rules: {
                 path: "notARealPath"
             },
-            expectedResult: ""
+            expectedResult: "",
+            expectedInverseResult: []
         },
-        {
+        "test_11": {
             input: ["Shyguy", "Rootbeer", "", false, {}, []],
             rules: {},
-            expectedResult: "Shyguy, Rootbeer"
+            expectedResult: "Shyguy, Rootbeer",
+            expectedInverseResult: ["Shyguy", "Rootbeer"]
         },
-        {
+        "test_12": {
             input: [],
             rules: {},
-            expectedResult: ""
+            expectedResult: "",
+            expectedInverseResult: []
         },
-        {
+        "test_13": {
             input: 0,
             rules: {},
-            expectedResult: ""
+            expectedResult: "",
+            expectedInverseResult: []
         },
-        {
+        "test_14": {
             input: {},
             rules: {},
-            expectedResult: ""
+            expectedResult: "",
+            expectedInverseResult: []
         },
-        {
+        "test_15": {
             input: "",
             rules: {},
-            expectedResult: ""
+            expectedResult: "",
+            expectedInverseResult: []
         },
-        {
+        "test_16": {
             input: null,
             rules: {},
-            expectedResult: ""
+            expectedResult: "",
+            expectedInverseResult: []
         },
-        {
+        "test_17": {
             input: undefined,
             rules: {},
-            expectedResult: undefined
+            expectedResult: undefined,
+            expectedInverseResult: undefined
         }
-    ];
+    };
 
     jqUnit.test("Test arrayToString transform function", function () {
-        jqUnit.expect(arrayToStringTransformTestCases.length);
+        jqUnit.expect(34);
 
         fluid.each(arrayToStringTransformTestCases, function (testCase, index) {
-            var transformRules = $.extend({}, testCase.rules, {
+            var transformSpec = $.extend({}, testCase.rules, {
                 type: "sjrk.storyTelling.transforms.arrayToString",
                 inputPath: "sourceArray"
             });
 
+            var transformRules = {
+                transform: transformSpec
+            };
+
             var resultString = fluid.model.transformWithRules(
                 { sourceArray: testCase.input },
-                { resultString: { transform: transformRules }}
+                { resultString: transformRules }
             ).resultString;
-
             jqUnit.assertEquals("Generated string value for test case " + index + " is as expected", testCase.expectedResult, resultString);
+
+            var invertedRules = fluid.model.transform.invertConfiguration(transformRules);
+            var invertedOutput = fluid.model.transformWithRules(
+                resultString,
+                invertedRules
+            ).sourceArray;
+            jqUnit.assertDeepEq("Generated inverted array values for test case " + index + " are as expected", testCase.expectedInverseResult, invertedOutput);
         });
     });
 
