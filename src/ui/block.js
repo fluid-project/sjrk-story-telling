d1ce87fa05a70ccb9507b82d9d73d2a17e92bf44
BlueSlug <mossmonkey@gmail.com>
2020-01-29 13:05:01 -0500

    SJRK-267: consolidated copyright dates in new AUTHORS.md, added refs
    (cherry picked from commit a9480a9af9c1f25f7d13e450905281e20ae168af)

--- src/ui/block.js
+++ src/ui/block.js
@@ -1,5 +1,8 @@
 /*
-Copyright 2018 OCAD University
+Copyright The Storytelling Tool copyright holders
+See the AUTHORS.md file in the docs directory of this distribution and at
+https://github.com/fluid-project/sjrk-story-telling/blob/master/docs/AUTHORS.md
+
 Licensed under the New BSD license. You may not use this file except in compliance with this licence.
 You may obtain a copy of the BSD License at
 https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENSE.txt
