28f68b89159c5a3602884e3d7f94ceac60781ca4
BlueSlug <mossmonkey@gmail.com>
2020-06-19 12:58:07 -0400

    SJRK-288: split ui grade tests off from storyUi grade tests

---
+++ tests/ui/js/ui-storyUiTests.js
@@ -0,0 +1,117 @@
+/*
+For copyright information, see the AUTHORS.md file in the docs directory of this distribution and at
+https://github.com/fluid-project/sjrk-story-telling/blob/master/docs/AUTHORS.md
+
+Licensed under the New BSD license. You may not use this file except in compliance with this licence.
+You may obtain a copy of the BSD License at
+https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENSE.txt
+*/
+
+/* global fluid, sjrk, jqUnit */
+
+"use strict";
+
+(function ($, fluid) {
+
+    // Test component for the UI grade
+    fluid.defaults("sjrk.storyTelling.ui.testStoryUi", {
+        gradeNames: ["sjrk.storyTelling.ui.storyUi"],
+        events: {
+            blockCreateEvent: null
+        },
+        blockTypeLookup: {
+            "cat": "cat.grade.name",
+            "otherCat": "cat.other.grade.name"
+        },
+        storyBlocks: [
+            {
+                blockType: "cat",
+                fur: "orange"
+            },
+            {
+                blockType: "cat",
+                fur: "spotted"
+            },
+            {
+                blockType: "otherCat",
+                fur: "none"
+            }
+        ],
+        components: {
+            templateManager: {
+                options: {
+                    templateConfig: {
+                        templatePath: "%resourcePrefix/templates/storyViewer.handlebars",
+                        resourcePrefix: "../.."
+                    }
+                }
+            }
+        }
+    });
+
+    // Test cases and sequences for the UI grade
+    fluid.defaults("sjrk.storyTelling.ui.storyUiTester", {
+        gradeNames: ["fluid.test.testCaseHolder"],
+        modules: [{
+            name: "Test Story UI.",
+            tests: [{
+                name: "Test createBlocksFromData function",
+                expect: 6,
+                sequence: [{
+                    funcName: "sjrk.storyTelling.ui.createBlocksFromData",
+                    args: ["{ui}.options.storyBlocks", "{ui}.options.blockTypeLookup", "{ui}.events.blockCreateEvent"]
+                },
+                {
+                    event: "{ui}.events.blockCreateEvent",
+                    listener: "sjrk.storyTelling.ui.storyUiTester.verifyBlocksCreated",
+                    args: ["{arguments}.0", "{arguments}.1", "cat.grade.name", {blockType: "cat", fur: "orange"}]
+                },
+                {
+                    event: "{ui}.events.blockCreateEvent",
+                    listener: "sjrk.storyTelling.ui.storyUiTester.verifyBlocksCreated",
+                    args: ["{arguments}.0", "{arguments}.1", "cat.grade.name", {blockType: "cat", fur: "spotted"}]
+                },
+                {
+                    event: "{ui}.events.blockCreateEvent",
+                    listener: "sjrk.storyTelling.ui.storyUiTester.verifyBlocksCreated",
+                    args: ["{arguments}.0", "{arguments}.1", "cat.other.grade.name", {blockType: "otherCat", fur: "none"}]
+                }]
+            }]
+        }]
+    });
+
+    /**
+     * Verifies that a block has been created as expected
+     *
+     * @param {Array} gradeNames - the actual gradeNames of the created block
+     * @param {Object} blockData - the model data of the given block
+     * @param {Array} expectedGradeNames - the expected grade names
+     * @param {Object} expectedModelValues - the expected model values
+     */
+    sjrk.storyTelling.ui.storyUiTester.verifyBlocksCreated = function (gradeNames, blockData, expectedGradeNames, expectedModelValues) {
+        jqUnit.assertEquals("The grade names are as expected", expectedGradeNames, gradeNames);
+        jqUnit.assertDeepEq("The model values are as expected", expectedModelValues, blockData.modelValues);
+    };
+
+    // Test environment
+    fluid.defaults("sjrk.storyTelling.storyUiTest", {
+        gradeNames: ["fluid.test.testEnvironment"],
+        components: {
+            storyUi: {
+                type: "sjrk.storyTelling.ui.testStoryUi",
+                container: "#testStoryUi",
+                createOnEvent: "{storyUiTester}.events.onTestCaseStart"
+            },
+            storyUiTester: {
+                type: "sjrk.storyTelling.ui.storyUiTester"
+            }
+        }
+    });
+
+    $(document).ready(function () {
+        fluid.test.runTests([
+            "sjrk.storyTelling.storyUiTest"
+        ]);
+    });
+
+})(jQuery, fluid);
