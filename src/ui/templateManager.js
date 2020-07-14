4f4568940fb1ea150015cf40b4b7d4b5a39e3cf3
BlueSlug <mossmonkey@gmail.com>
2020-07-14 07:18:19 -0400

    SJRK-172: automatically detect and convert URLs to links in text blocks

--- src/ui/templateManager.js
+++ src/ui/templateManager.js
@@ -111,6 +111,10 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
             templateRenderer: {
                 type: "gpii.handlebars.renderer",
                 options: {
+                    distributeOptions: {
+                        record: true,
+                        target: "{that gpii.handlebars.helper.md}.options.markdownItOptions.linkify"
+                    },
                     components: {
                         getIds: {
                             type: "sjrk.storyTelling.templateManager.getIdsHelper"
