fluid-project/sjrk-story-telling

commit 77c8ed52a867aabc0c115059a3a3cd48aac8e92c

SJRK-289: fixed bug in uploader's valueMapper model relay specification

src/ui/blockUi-editor.js
@@ -193,7 +193,6 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
                             singleTransform: {
                                 type: "fluid.transforms.valueMapper",
                                 defaultInputPath: "uploadState",
-                                outputPath: "{editor}.model",
                                 match: {
                                     "ready": {
                                         outputValue: {

commit d8de999f320e05ac82102c448a36f8573436b5fd

SJRK-289: fix bug introduced in 77c8ed5, correct test count (timestamp)
The moveBlock disabled states were backfeeding into the block order, 
causing a model relay loop. How this wasn't caught by the tests is 
beyond me!

src/ui/blockUi-editor.js
@@ -20,12 +20,26 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
             // the ID of the story of which the block being edited is a part
             storyId: ""
         },
-        model: {
+        modelRelay: {
             // reorder buttons are disabled depending on the block's order:
-            // if the block is first in the order, the "up" button is disabled
             // if the block is last in the order, the "down" button is disabled
-            moveBlockDownDisabled: "{block}.model.lastInOrder",
-            moveBlockUpDisabled: "{block}.model.firstInOrder"
+            blockLastInOrderToDownButtonDisabled: {
+                source: "{block}.model.lastInOrder",
+                target: "moveBlockDownDisabled",
+                singleTransform: {
+                    type: "fluid.transforms.value"
+                },
+                backward: "never"
+            },
+            // if the block is first in the order, the "up" button is disabled
+            blockFirstInOrderToUpButtonDisabled: {
+                source: "{block}.model.firstInOrder",
+                target: "moveBlockUpDisabled",
+                singleTransform: {
+                    type: "fluid.transforms.value"
+                },
+                backward: "never"
+            }
         },
         modelListeners: {
             // The use of excludeSource: ["init"] in these two model listeners

tests/ui/js/ui-storyUiTests.js
@@ -164,7 +164,7 @@ https://raw.githubusercontent.com/fluid-project/sjrk-story-telling/master/LICENS
             },
             {
                 name: "Test block model listener",
-                expect: 4,
+                expect: 3,
                 sequence: [{
                     funcName: "jqUnit.assertDeepEq",
                     args: ["Story model is as expected before model change is triggered", sjrk.storyTelling.ui.testStoryUi.testStoryInitial, "{storyUi}.story.model"]