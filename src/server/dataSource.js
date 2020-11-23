21354c750e26de70620cc8602301e3434868aa49
BlueSlug <mossmonkey@gmail.com>
2020-11-23 18:38:29 -0500

    SJRK-425: add schemaVersion to all new database record writes
    This way of including the value is not ideal, but it is currently the 
    only functioning solution.
    
    This commit will also remove an unused requestHandler parameter leftover 
    from a previous factoring

--- src/server/dataSource.js
+++ src/server/dataSource.js
@@ -13,6 +13,11 @@ var fluid = require("infusion");
 require("kettle");
 
 var sjrk = fluid.registerNamespace("sjrk");
+fluid.registerNamespace("sjrk.storyTelling.server.dataSource.couch.story");
+
+// This is the data schema version for the project.
+// All new stories will have their version set to this value.
+sjrk.storyTelling.server.dataSource.couch.story.schemaVersion = "0.4.0";
 
 // A CouchDB-compatible Kettle DataSource, providing core functionality
 fluid.defaults("sjrk.storyTelling.server.dataSource.couch.core", {
@@ -61,7 +66,7 @@ fluid.defaults("sjrk.storyTelling.server.dataSource.couch.view", {
 
 // A CouchDB DataSource for a single story, for use on the storyView and storyEdit pages
 fluid.defaults("sjrk.storyTelling.server.dataSource.couch.story", {
-    gradeNames: ["sjrk.storyTelling.server.dataSource.couch.core", "kettle.dataSource.CouchDB"],
+    gradeNames: ["fluid.modelComponent", "sjrk.storyTelling.server.dataSource.couch.core", "kettle.dataSource.CouchDB"],
     rules: {
         writePayload: {
             type: {
@@ -70,7 +75,13 @@ fluid.defaults("sjrk.storyTelling.server.dataSource.couch.story", {
                     input: "story"
                 }
             },
-            value: ""
+            value: "",
+            schemaVersion: {
+                transform: {
+                    type: "fluid.transforms.literalValue",
+                    input: sjrk.storyTelling.server.dataSource.couch.story.schemaVersion
+                }
+            }
         },
         readPayload: {
             "_rev": "_rev",
