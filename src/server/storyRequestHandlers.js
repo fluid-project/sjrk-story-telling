1c6a696caec38a89cd3219731d4cd78059ede643
BlueSlug <mossmonkey@gmail.com>
2021-02-17 19:07:25 -0500

    NOJIRA: fix bug that excludes the author from story data on View page

--- src/server/storyRequestHandlers.js
+++ src/server/storyRequestHandlers.js
@@ -118,7 +118,7 @@ sjrk.storyTelling.server.handleGetStory = function (request, dataSource) {
     promise.then(function (response) {
         if (response.published) {
             // remove authorID from the story model before sending
-            var storyModel = fluid.censorKeys(response, "authorID");
+            var storyModel = fluid.censorKeys(response, ["authorID"]);
             request.events.onSuccess.fire(JSON.stringify(storyModel));
         } else {
             fluid.log(fluid.logLevel.WARN, "Unauthorized: cannot access an unpublished story: " + id);
